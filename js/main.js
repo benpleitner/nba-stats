b31f1ec9480b11018bdc1d40d511fec0f92602c3
Ben <benpleitner@gmail.com>
2016-01-03 18:21:24 -0500

    added spinner

--- js/main.js
+++ js/main.js
@@ -2,6 +2,31 @@ console.log("TEAM: " + amplify.store("team") + " DATA: " + amplify.store("team_d
 
 $("#team_name_title").text(amplify.store("team") + " Player Data");
 
+//Start the spinner and hide the content
+var opts = {
+  lines: 13, // The number of lines to draw
+  length: 20, // The length of each line
+  width: 10, // The line thickness
+  radius: 30, // The radius of the inner circle
+  corners: 1, // Corner roundness (0..1)
+  rotate: 0, // The rotation offset
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#03b7f9', // #rgb or #rrggbb or array of colors
+  speed: 1, // Rounds per second
+  trail: 60, // Afterglow percentage
+  shadow: false, // Whether to render a shadow
+  hwaccel: false, // Whether to use hardware acceleration
+  className: 'spinner', // The CSS class to assign to the spinner
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  top: '50%', // Top position relative to parent
+  left: '50%' // Left position relative to parent
+};
+
+$(".container").css("display", "none");
+
+var target = document.getElementById("spin");
+var spinner = new Spinner(opts).spin(target);
+
 d3.csv("../data/" + amplify.store("team_data"), function(error, data) {
   console.log(data);
 
@@ -248,6 +273,9 @@ function makePPGRowChart(graph_data, ndx) {
         .transitionDuration(700);
 
   dc.renderAll();
+
+  spinner.stop();
+  $(".container").css("display", "inherit");
 };
 
 function makeAPGRowChart(graph_data, ndx) {
